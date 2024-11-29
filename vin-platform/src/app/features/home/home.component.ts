9c638428f4ef77e1c062c25fcb1fedded6fbb869
Pablo <pablocamacho92@gmail.com>
2024-11-29 11:04:11 -0300

    Social links

--- vin-platform/src/app/features/home/home.component.ts
+++ vin-platform/src/app/features/home/home.component.ts
@@ -3,11 +3,12 @@ import { CoverPhotoComponent } from "./cover-photo/cover-photo.component";
 import { ProfilePhotoComponent } from "./profile-photo/profile-photo.component";
 import { PreviewCarouselComponent } from "./preview-carousel/preview-carousel.component";
 import { PricesComponent } from "./prices/prices.component";
+import { SocialLinksComponent } from "./social-links/social-links.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CoverPhotoComponent, ProfilePhotoComponent, PreviewCarouselComponent, PricesComponent],
+  imports: [CoverPhotoComponent, ProfilePhotoComponent, PreviewCarouselComponent, PricesComponent, SocialLinksComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
